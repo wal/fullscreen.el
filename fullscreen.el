cd51e10b810fce0cf6c5817b4d2b0488fb1b7e55
wal <walter.mcconnell@gmail.com>
2010-11-01 20:48:20 +0000

    first commit

---
+++ fullscreen.el
@@ -0,0 +1,11 @@
+;; Fullscreen mode
+(defun toggle-fullscreen (&optional f)
+  (interactive)
+  (let ((current-value (frame-parameter nil 'fullscreen)))
+    (set-frame-parameter nil 'fullscreen
+                         (if (equal 'fullboth current-value)
+                             (if (boundp 'old-fullscreen) old-fullscreen nil)
+                           (progn (setq old-fullscreen current-value)
+                                  'fullboth)))))
+
+(add-hook 'after-make-frame-functions 'toggle-fullscreen)
